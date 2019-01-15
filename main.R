ae2a80ab0c2ffaaff1b25b5614429fac31778d08
Marrit <marrit.leenstra@wur.nl>
2019-01-15 11:50:59 +0100

    municipality with max NDVI calculated

--- main.R
+++ main.R
@@ -25,22 +25,21 @@ nlMunicipality <- getData('GADM',country='NLD', level=2, path = "data")
 
 # load data to global environment
 path <- list.files(path = "data/", pattern = glob2rx('*.grd'), full.names = TRUE)
-modis2 <- stack(path)
 modis <- brick(path)
 
 # transform to equal coordinate systems and mask
 nlMunicipalityPro <- spTransform(nlMunicipality, crs(proj4string(modis)))
 maskModis <- mask(modis, nlMunicipalityPro)
 
-nlMunicipalityPro@data <- nlMunicipalityPro@data[!is.na(nlMunicipalityPro$NAME_2),] # Remove rows with NA
+#nlMunicipalityPro@data <- nlMunicipalityPro@data[!is.na(nlMunicipalityPro$NAME_2),] # Remove rows with NA
 
+munNDVI <- extract(maskModis, nlMunicipalityPro, fun = mean, df = TRUE)
 
-munNDVI <- extract(maskModis, layer = 1, nlMunicipalityPro, fun = mean, df = TRUE)
-munNDVI2 <- cbind(nlMunicipalityPro$NAME_2, munNDVI)
-colnames(munNDVI2[1]) <- "municipality"
+munNDVI2 <- munNDVI
+munNDVI2$municipality <- nlMunicipality$NAME_2
 
-which(munNDVI == max(munNDVI[!is.na(munNDVI)]))
+munNDVI[(max(munNDVI[!is.na(munNDVI)])),]
+max <- which(munNDVI2$January == max(munNDVI2$January[!is.na(munNDVI2$January)]))
 
-
-
-
+munMaxJan <- munNDVI2[which(munNDVI2$January == max(munNDVI2$January[!is.na(munNDVI2$January)])),'municipality']
+munMaxAugust <- munNDVI2[which(munNDVI2$August == max(munNDVI2$August[!is.na(munNDVI2$August)])),'municipality']
