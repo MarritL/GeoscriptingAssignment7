d61d01fe0338929f0a17fea11502e0b0aeb5d2e7
Marrit <marrit.leenstra@wur.nl>
2019-01-15 16:53:28 +0100

    function to calculate which municipality has highest NDVI, debugged province plotting

--- main.R
+++ main.R
@@ -8,13 +8,14 @@
 # load libraries
 if (!require("sp")) install.packages("sp") 
 if (!require("raster")) install.packages("raster")
-if (!require("dplyr")) install.package("dplyr")
+if (!require("dplyr")) install.packages("dplyr")
 library(sp)
 library(raster)
 library(dplyr)
 
 # source functions
 source("R/retrieveData.R")
+source("R/highestNDVI.R")
 
 # download and unzip data 
 retrieveData("https://raw.githubusercontent.com/GeoScripting-WUR/VectorRaster/gh-pages/data/MODIS.zip", "data")
@@ -32,17 +33,28 @@ maskModis <- mask(modis, nlMunicipalityPro)
 munNDVI <- extract(maskModis, nlMunicipalityPro, fun = mean, df = TRUE, na.rm = TRUE)
 munNDVI$municipality <- nlMunicipality$NAME_2
 
-# check which municipality has the max NDVI
-munMaxJan <- munNDVI[which(munNDVI$January == max(munNDVI$January[!is.na(munNDVI$January)])),'municipality']
-munMaxAugust <- munNDVI[which(munNDVI$August == max(munNDVI$August[!is.na(munNDVI$August)])),'municipality']
-munNDVI$mean <- rowMeans(munNDVI[,2:13], na.rm = TRUE) 
-munMaxYear <- munNDVI[which(munNDVI$mean == max(munNDVI$mean[!is.na(munNDVI$mean)])),'municipality']
+# check which municipality has the highest NDVI
+munMaxJan <- highestNDVI(munNDVI, "January") 
+munMaxAugust <- highestNDVI(munNDVI, "August") 
+munNDVI$mean <- rowMeans(munNDVI[,2:13], na.rm = TRUE) # add a column with yearly mean NDVI
+munMaxYear <- highestNDVI(munNDVI, "mean") 
+
+# calculate which province has the highest NDVI
+munNDVI$Province <- nlMunicipality$NAME_1
+munMeanProvince <- munNDVI %>% group_by(Province) %>% summarise(meanProvince = mean(January))
+munMaxProvince <- as.character(munMeanProvince[which(munMeanProvince$meanProvince == max(munMeanProvince$meanProvince)),'Province'])
+nlProvincePro <- raster::aggregate(nlMunicipalityPro, by = 'NAME_1', dissolve = TRUE)
 
 ## Set graphical parameters (one row and two columns)
 opar <- par(mfrow=c(1,2))
 
+# plot province with highest NDVI
+plot(nlProvincePro, lwd = 0.15, main = paste("Greenest Dutch province in January"), axes = TRUE)
+plot(nlProvincePro[nlProvincePro$NAME_1 == munMaxProvince,], add = TRUE, col = 'green', lwd = 0.1)
+mtext(side = 1, line = -1, "Coordinate system: Sinusoidal \n Authors: A.-J. Welsink, M. Leenstra ", adj = 1, cex = 0.4)
+legend("topleft", legend = "Utrecht", col = 'green', pch = 20, cex = 0.6)
 
-# plot
+# plot municipality with highest NDVI
 nlMunicipalityPro@data <- nlMunicipalityPro@data[!is.na(nlMunicipalityPro$NAME_2),]
 plot(nlMunicipalityPro, lwd = 0.1, main = paste("Greenest Dutch municipality in January, August, and year-round"), axes = TRUE)
 plot(nlMunicipalityPro[nlMunicipalityPro$NAME_2 == munMaxJan,], lwd = 0.1, add = TRUE, col = 'red')
@@ -51,14 +63,3 @@ plot(nlMunicipalityPro[nlMunicipalityPro$NAME_2 == munMaxYear,], lwd = 0.1, add 
 mtext(side = 1, line = -2, "Coordinate system: Sinusoidal \n Authors: A.-J. Welsink, M. Leenstra ", adj = 1, cex = 0.4)
 legend("topleft", legend = c(paste("Highest NDVI January: \n", munMaxJan), paste("\n Highest NDVI August: \n", munMaxAugust), paste("\n Highest mean NDVI year-round: \n", munMaxYear)), col = c('red', 'blue', 'green'), pch = 20, cex = 0.6, bty = 'n')
 
-# calculate which province has the highest NDVI
-munNDVI$Province <- nlMunicipality$NAME_1
-munMeanProvince <- munNDVI %>% group_by(Province) %>% summarise(meanProvince = mean(January))
-munMaxProvince <- as.character(munMeanProvince[which(munMeanProvince$meanProvince == max(munMeanProvince$meanProvince)),'Province'])
-nlProvincePro <- raster::aggregate(nlMunicipalityPro, by = 'NAME_1', dissolve = TRUE)
-
-# plot
-plot(nlProvincePro, lwd = 0.15, main = paste("Greenest Dutch province in January"), axes = TRUE)
-plot(nlProvincePro[nlProvincePro$NAME_1 == munMaxProvince,], add = TRUE, col = 'green', lwd = 0.1)
-mtext(side = 1, line = -1, "Coordinate system: Sinusoidal \n Authors: A.-J. Welsink, M. Leenstra ", adj = 1, cex = 0.4)
-legend("topleft", legend = "Utrecht", col = 'green', pch = 20, cex = 0.6)
