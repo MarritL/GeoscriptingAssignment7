6ab48b1cb6dd5e523a98c121bff224fb7e18e5df
Anne-Juul <anne-juul.welsink@wur.nl>
2019-01-15 16:06:41 +0100

    Includes plots of municipalities with highest NDVI year-round, in January, and August

--- main.R
+++ main.R
@@ -8,8 +8,8 @@
 # load libraries
 if (!require("sp")) install.packages("sp")
 if (!require("raster")) install.packages("raster")
-
-# library(sp)
+library(sp)
+library(raster)
 
 # source functions
 source("R/retrieveData.R")
@@ -27,15 +27,36 @@ nlMunicipality <- getData('GADM',country='NLD', level=2, path = "data")
 path <- list.files(path = "data/", pattern = glob2rx('*.grd'), full.names = TRUE)
 modis <- brick(path)
 
-# transform to equal coordinate system and mask
+# transform to equal coordinate system, mask, and crop
 nlMunicipalityPro <- spTransform(nlMunicipality, crs(proj4string(modis)))
 maskModis <- mask(modis, nlMunicipalityPro)
 
+
 # extract NDVI for each municipality
 munNDVI <- extract(maskModis, nlMunicipalityPro, fun = mean, df = TRUE)
-munNDVI2 <- munNDVI
-munNDVI2$municipality <- nlMunicipality$NAME_2
+munNDVI$municipality <- nlMunicipality$NAME_2
 
-# check which municipality has the highest NDVI
-munMaxJan <- munNDVI2[which(munNDVI2$January == max(munNDVI2$January[!is.na(munNDVI2$January)])),'municipality']
-munMaxAugust <- munNDVI2[which(munNDVI2$August == max(munNDVI2$August[!is.na(munNDVI2$August)])),'municipality']
+# check which municipality has the highest NDVI 
+munMaxJan <- munNDVI[which(munNDVI$January == max(munNDVI$January[!is.na(munNDVI$January)])),'municipality']
+munMaxAugust <- munNDVI[which(munNDVI$August == max(munNDVI$August[!is.na(munNDVI$August)])),'municipality']
+munNDVI$mean <- (rowMeans(munNDVI[,2:13], na.rm = TRUE))
+munMaxYear <- munNDVI[which(munNDVI$mean == max(munNDVI$mean[!is.na(munNDVI$mean)])),'municipality']
+
+# plot
+nlMunicipalityPro@data <- nlMunicipalityPro@data[!is.na(nlMunicipalityPro$NAME_2),]
+plot(nlMunicipalityPro, lwd = 0.1, main = paste("Greenest Dutch municipality in January, August, and year-round"), axes = TRUE)
+plot(nlMunicipalityPro[nlMunicipalityPro$NAME_2 == munMaxJan,], lwd = 0.1, add = TRUE, col = 'red')
+plot(nlMunicipalityPro[nlMunicipalityPro$NAME_2 == munMaxAugust,], lwd = 0.1, add = TRUE, col = 'blue')
+plot(nlMunicipalityPro[nlMunicipalityPro$NAME_2 == munMaxYear,], lwd = 0.1, add = TRUE, col = 'green')
+mtext(side = 1, line = -2, "Coordinate system: Sinusoidal \n Authors: A.-J. Welsink, M. Leenstra ", adj = 1, cex = 0.4)
+legend("topleft", legend = c(paste("Highest NDVI January: \n", munMaxJan), paste("\n Highest NDVI August: \n", munMaxAugust), paste("\n Highest mean NDVI year-round: \n", munMaxYear)), col = c('red', 'blue', 'green'), pch = 20, cex = 0.6, bty = 'n')
+
+
+
+
+
+
+
+
+
+
