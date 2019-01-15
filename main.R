7a5b62997b511f37f0d9019e6320afc946988076
Anne-Juul <anne-juul.welsink@wur.nl>
2019-01-15 08:40:53 +0100

    Initial commit

---
+++ main.R
@@ -0,0 +1,7 @@
+# Authors: Anne-Juul Welsink and Marrit Leenstra
+# 15th January 2019
+# Exercise 7, Geoscripting, Wageningen University 
+
+# This project maps which "municipality" in the Netherlands was the 
+# greenest in January, in August on average over the year. 
+
